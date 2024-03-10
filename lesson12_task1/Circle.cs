3e8a29c7d2747eb7d09b7ab03c420ef6b93c95d0
Asiya Ermolaeva <asiya_ermolaeva@mail.ru>
2024-03-10 20:21:57 +0300

    Добавьте файлы проекта.

---
+++ lesson12_task1/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson12_task1
+{
+    static class Circle
+    {
+        static public double GetSquare(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+
+        static public double GetCircumference(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+
+        static public bool DoesPointBelongToCircle(double x0, double y0, double r, double x, double y)
+        {
+            double distance = Math.Sqrt(Math.Pow(x - x0, 2) + Math.Pow(y - y0, 2));
+            return distance <= r;
+        }
+    }
+}
---
+++ lesson12_task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson12_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты центра круга (x0,y0)");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите радиус круга");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки (x,y), \n пренадлежность кругу которой необходимо проверить ");
+
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Длина окружности = {0}, площать круга = {1}, принадлежит точка кругу - {2}", 
+                Circle.GetCircumference(r),
+                Circle.GetSquare(r),
+                (Circle.DoesPointBelongToCircle(x0,y0,r,x,y))?"да":"нет");
+            Console.ReadKey();
+        }
+
+    }
+}
